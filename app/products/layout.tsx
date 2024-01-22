929313ef46751caf88ceecf755d1135f0dc5e3b7
Vidssk <santiagohernandez120199@gmail.com>
2024-01-22 01:57:12 -0600

    Redo Portfolio Complete

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 // Layout for reusable components in eaxh /products
-export const Layout = () => {
+export const ProductLayout = () => {
   return (
     <div>layout</div>
   )
