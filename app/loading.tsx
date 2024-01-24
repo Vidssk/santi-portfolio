7f64eb6543b76ff82c0486431601ac2659c07ec5
Vidssk <santiagohernandez120199@gmail.com>
2024-01-23 18:27:11 -0600

    Fixed loading Page

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const loading = () => {
   return (
     // You can add any UI inside Loading, including a skeleton.
-    <div className='app'>Loading</div>
+    <div className='loading-page'>Loading</div>
   );
 }
 
