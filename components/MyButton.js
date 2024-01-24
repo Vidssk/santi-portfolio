67028505b5641237853d3df58d6130086914b048
Vidssk <santiagohernandez120199@gmail.com>
2024-01-23 19:24:29 -0600

    Resume Updated

--- components/MyButton.js
+++ components/MyButton.js
@@ -7,7 +7,7 @@ import Resume from '@public/Santiagos_Resume.pdf'
 function MyButton() {
   return (
     // <a href={Resume} className="my-resume" download="Santiagos_Resume.pdf"> Download Resume </a>
-    <a href="https://drive.google.com/file/d/1NCAwDqMypEP15h0s_MNy7RZuvcZXK5bw/view?usp=sharing" 
+    <a href="https://drive.google.com/file/d/1ev2GW70kNzE_RbGC7bHVroUQZn0Ygnn2/view?usp=sharing" 
     className="my-button" target="_blank" rel="noopener noreferrer" ><button>Download Resume</button></a>
     )
 }
